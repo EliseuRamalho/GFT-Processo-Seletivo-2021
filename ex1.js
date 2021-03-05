f7077204bec7a0801f41271f85cc422ec033cbd7
EliseuRamalho <zeu.ramalho@hotmail.com>
2021-03-05 15:38:25 -0300

    Exercicio da Classe Pai Investimento

---
+++ ex1.js
@@ -0,0 +1,11 @@
+//Classe pai Investimento
+class Investimento {
+    //Construtor passando os parâmentros que serão utilizados
+    constructor(valor, jurosMensais) {
+      //Setando os parâmentros
+      this.valor = valor;
+      this.jurosMensais = jurosMensais;
+    }
+  }
+  //Função calcularLucro
+  function calcularLucro()
